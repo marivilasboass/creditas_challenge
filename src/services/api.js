20d90db6fe562019f5f1b2ae68e342f2167ad4a0
Mari <mari@MacBook-Pro-de-Mari.local>
2019-11-19 13:54:31 -0300

    create a service api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,39 @@
-import axios from 'axios';
+const handleResponse = async response => {
+  if (response.status) {
+    const data = await response.json();
 
-const api = axios.create({
-  baseURL: 'https://api.github.com',
-});
+    if (response.status !== 200) {
+      return Promise.reject(data);
+    }
+    return Promise.resolve(data);
+  }
+  return Promise.reject(response);
+};
 
-export default api;
+const fetchUser = ({email, password, credential}) => {
+  return fetch(`${'https://api.github.com'}/user`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+      Authorization: `Basic ${credential}`,
+    },
+  }).then(handleResponse);
+};
+
+const fetchDynamic = ({url, credential}) => {
+  const normalizedUrl = String(url).replace('{/sha}', '');
+  return fetch(normalizedUrl, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+      Authorization: `Basic ${credential}`,
+    },
+  }).then(handleResponse);
+};
+
+export default {
+  fetchUser,
+  fetchDynamic,
+};
