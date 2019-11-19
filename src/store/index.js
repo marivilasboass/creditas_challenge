c94c9bb395406c0157a872b042d2ca259d171a5d
Mari <mari@MacBook-Pro-de-Mari.local>
2019-11-19 14:18:21 -0300

    create redux

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import rootReducer from './Ducks';
+import ReactotronConfig from '~/config/ReactotronConfig';
+
+const middlewares = [];
+const enhancers = [];
+
+enhancers.push(applyMiddleware(thunk, ...middlewares));
+
+const createAppropriateStore = createStore;
+
+const persistConfig = {
+  key: 'v1.0',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createAppropriateStore(
+  persistedReducer,
+  compose(
+    ...enhancers,
+    ReactotronConfig.createEnhancer(),
+  ),
+);
+
+const persistor = persistStore(store);
+
+export {store, persistor};
